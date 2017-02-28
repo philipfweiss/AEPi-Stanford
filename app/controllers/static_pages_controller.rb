0a3626e081882a60dc3c8dd6d860f64dd4e800d8
philipfweiss <weissp68@stanford.edu>
2017-02-27 18:09:15 -0800

    add static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,26 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def rush
+  end
+
+  def photos
+  end
+
+  def past_events
+  end
+
+  def current
+  end
+
+  def alumni
+  end
+
+  def about
+  end
+
+  def contact_us
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'static_pages/rush'
+
+  get 'home' => 'static_pages#home'
+  get 'rush' => 'static_pages#rush'
+  get 'photos' => 'static_pages#photos'
+  get 'past-events' => 'static_pages#past_events'
+  get 'current' => 'static_pages#current'
+  get 'alumni' => 'static_pages#alumni'
+  get 'about' => 'static_pages#about'
+  get 'contact-us' => 'static_pages#contact_us'
+  get 'home' => 'static_pages#home'
+
   root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
