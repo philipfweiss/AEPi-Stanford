70472b8933dc77f2ec56c4a1d6a101c7bb849e5f
philipfweiss <weissp68@stanford.edu>
2017-03-05 04:14:10 -0800

    update brotherhood

--- app/controllers/brothers_only_controller.rb
+++ app/controllers/brothers_only_controller.rb
@@ -1,4 +1,11 @@
 class BrothersOnlyController < ApplicationController
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == 'sigmatau' && password == 'rimram'
+    end
+  end
   def home
 
   end
